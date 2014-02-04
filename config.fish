eefba7ec90a0fa09b77f6e5684aafbf4bdf4591c
David Reid <dreid@dreid.org>
2014-02-04 13:55:28 -0800

    Fix path handling, add gopath, add dvm support.

--- config.fish
+++ config.fish
@@ -1,6 +1,7 @@
 set fish_greeting ""
 
-set fish_user_paths ~/Library/Python/2.7/bin ~/.local/bin/ /usr/local/share/npm/bin
+set fish_user_paths ~/Library/Python/2.7/bin ~/.local/bin
+set fish_user_paths /usr/local/share/npm/bin $fish_user_paths
 
 set -g __fish_git_prompt_show_informative_status 1
 set -g __fish_git_prompt_showuntrackedfiles 1
@@ -9,12 +10,21 @@ set -g __fish_git_prompt_showstashstate 1
 set -g __fish_git_prompt_showcolorhints 1
 
 set -gx EDITOR "subl -w"
+set -gx GOPATH $HOME/code/go
+
+set fish_user_paths $GOPATH/bin $fish_user_paths
 
 # rbenv
 
 if test -f (which rbenv)
     set fish_user_paths $HOME/.rbenv/shims $fish_user_paths
     rbenv rehash >/dev/null ^&1
+end
+
+# dvm
+
+if test -f (which dvm)
+    eval (dvm env)
 end
 
 # Virtualfish
