27975d2c4dbdc1617a2f5ab02f04c314b2b158f4
David Reid <dreid@dreid.org>
2014-03-26 02:32:44 -0700

    fix md5 hash_color on linux.

--- functions/hash_color.fish
+++ functions/hash_color.fish
@@ -1,3 +1,8 @@
 function hash_color
-	echo $argv[1] | md5 | cut -c1-6
+    set -l md5 md5
+    if not which md5 > /dev/null
+        set -l md5 md5sum
+    end
+
+	echo $argv[1] | eval $md5 | cut -c1-6
 end
