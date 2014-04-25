eba0cef3ae77abe617e2f228cc359f8773388102
David Reid <dreid@dreid.org>
2014-04-24 23:39:47 -0700

    Optional stuff

--- config.fish
+++ config.fish
@@ -4,9 +4,18 @@ set fish_color_user 05f
 set fish_color_status f00
 
 set fish_user_paths ~/Library/Python/2.7/bin ~/.local/bin
-set fish_user_paths /usr/local/share/npm/bin $fish_user_paths
-set fish_user_paths ~/.cabal/bin $fish_user_paths
-set fish_user_paths ~/Applications/git-annex.app/Contents/MacOS $fish_user_paths
+
+if test -d /usr/local/share/npm/bin
+    set fish_user_paths /usr/local/share/npm/bin $fish_user_paths
+end
+
+if test -d ~/.cabal/bin
+    set fish_user_paths ~/.cabal/bin $fish_user_paths
+end
+
+if test -d ~/Applications/git-annex.app
+    set fish_user_paths ~/Applications/git-annex.app/Contents/MacOS $fish_user_paths
+end
 
 set -g __fish_git_prompt_show_informative_status 1
 set -g __fish_git_prompt_showuntrackedfiles 1
@@ -17,11 +26,15 @@ set -g __fish_git_prompt_showcolorhints 1
 set -gx EDITOR "subl -w"
 set -gx GOPATH $HOME/code/go
 
-set fish_user_paths $GOPATH/bin $fish_user_paths
+if test -d $GOPATH/bin
+    set fish_user_paths $GOPATH/bin $fish_user_paths
+end
 
 # direnv
 
-eval (direnv hook fish)
+if test -f (which direnv)
+    eval (direnv hook fish)
+end
 
 # rbenv
 
